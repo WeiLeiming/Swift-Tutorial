84223ec4445953f2f0ceca27ffbb87a22b560ed6
WillWei <2008weileiming@163.com>
2017-02-24 14:43:06 +0800

    Update SwiftTutorial to A swift Tour

--- A Swift Tour/A Swift Tour.playground/Contents.swift
+++ A Swift Tour/A Swift Tour.playground/Contents.swift
@@ -373,4 +373,92 @@ var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
 
-		
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+print(7.simpleDescription)
+
+let protocolValue: ExampleProtocol = a
+print(protocolValue.simpleDescription)
+
+enum PrinterError: Error {
+    case OutOfPaper
+    case NoToner
+    case OnFire
+}
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.NoToner
+    }
+    return "Job sent"
+}
+
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try send(job: 1440, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch PrinterError.OnFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+    let result = fridgeContent.contains(food)
+    return result
+}
+fridgeContains("banana")
+print(fridgeIsOpen)
+
+func repeatItem<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+repeatItem(repeating: "knock", numberOfTimes: 4)
+
+enum OptionalValue<Wrapped> {
+    case None
+    case Some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+}
+anyCommonElements([1, 2, 3], [3])
--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 ### 主要资料
 
-官方 Swift 教程《The Swift Programming Language》翻译[开源项目](https://github.com/numbbbbb/the-swift-programming-language-in-chinese)
+苹果官方 Swift 教程The Swift Programming Language的中文翻译开源[项目](https://github.com/numbbbbb/the-swift-programming-language-in-chinese)
 
-[中文版](http://wiki.jikexueyuan.com/project/swift/) 	[英文原版](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/index.html#//apple_ref/doc/uid/TP40014097-CH3-ID0)
+The Swift Programming Language		---->		[中文版](http://wiki.jikexueyuan.com/project/swift/) 	[英文原版](https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/index.html#//apple_ref/doc/uid/TP40014097-CH3-ID0)
