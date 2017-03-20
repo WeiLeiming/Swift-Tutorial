40d15a4c14811816f0a254ae586821fd895dda79
WillWei <2008weileiming@163.com>
2017-03-20 11:10:00 +0800

    update Functions & add Closures

--- Functions/Functions.playground/Contents.swift
+++ Functions/Functions.playground/Contents.swift
@@ -89,3 +89,110 @@ if let bounds = minMax1(array: [8, -6, 2, 109, 3, 71]) {
 }
 
 // 函数参数标签和参数名称
+func someFunction(firstParameterName: Int, secondParameterName: Int) {
+    
+}
+someFunction(firstParameterName: 1, secondParameterName: 2)
+
+func someFunction(argumentLabel parameterName: Int) {
+    
+}
+
+func greet(person: String, from hometown: String) -> String {
+    return "Hello \(person)! Glad you could visit from \(hometown)."
+}
+print(greet(person: "Bill", from: "Cupertion"))
+
+func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
+    
+}
+someFunction(1, secondParameterName: 2)
+
+func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+    
+}
+
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6)
+someFunction(parameterWithoutDefault: 4)
+
+func artimeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+artimeticMean(1, 2, 3, 4, 5)
+artimeticMean(3, 8.25, 18.75)
+
+// 输入输出参数
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+// 函数类型
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+func printHelloWorld() {
+    print("Hello, world")
+}
+
+// 使用函数类型
+var mathFunction: (Int , Int) -> Int = addTwoInts
+print("Result: \(mathFunction(2, 3))")
+mathFunction = multiplyTwoInts
+print("Result: \(mathFunction(2, 3))")
+let anotherFunction = addTwoInts
+
+// 函数类型作为参数类型
+func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(mathFunction(a, b))")
+}
+printMathResult(addTwoInts, 3, 5)
+
+// 函数类型作为返回类型
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+
+var currentValue = 3
+var moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+
+print("Counting to zero:")
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
+}
+
+// 嵌套函数
+func chooseStepFunction1(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+currentValue = -4
+moveNearerToZero = chooseStepFunction1(backward: currentValue > 0)
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+		
