b83acedc61dd9b18ad3a1a0e1bf31eb2cf71afe1
WillWei <2008weileiming@163.com>
2017-03-21 11:45:32 +0800

    update Closures & add Enumerations

--- Closures/Closures.playground/Contents.swift
+++ Closures/Closures.playground/Contents.swift
@@ -3,3 +3,155 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// sorted 方法
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by: backward);
+
+// 闭包表达式语法
+/*
+{ (parameters) -> returnType in
+    statements
+}
+ */
+
+reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+})
+
+// 根据上下文推断类型
+reversedNames = names.sorted(by: { s1, s2 in return s1 > s2 })
+print(reversedNames)
+
+// 单表达式闭包隐式返回
+reversedNames = names.sorted(by: { s1, s2 in s1 > s2 })
+print(reversedNames)
+
+// 参数名称缩写
+reversedNames = names.sorted(by: { $0 > $1 })
+
+// 运算符方法
+reversedNames = names.sorted(by: >)
+print(reversedNames)
+
+// 尾随闭包
+func someFunctionThatTakesAClosure(closure: () -> Void) {
+    
+}
+
+someFunctionThatTakesAClosure(closure: {
+
+})
+
+someFunctionThatTakesAClosure() {
+    
+}
+
+reversedNames = names.sorted() { $0 > $1 }
+reversedNames = names.sorted { $0 > $1 }
+
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+let numbers = [26, 58, 510]
+
+
+let strings = numbers.map {
+    (number) -> String in
+    var number = number
+    var output = ""
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    return output
+}
+print(strings)
+
+// 值捕获
+func makeIncrementer(forIncrement amout: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amout
+        return runningTotal
+    }
+    return incrementer
+}
+
+let incrementByTen = makeIncrementer(forIncrement: 10)
+incrementByTen()
+incrementByTen()
+
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+incrementBySeven()
+
+// 闭包是引用类型
+let alsoIncrementByTen = incrementByTen
+alsoIncrementByTen()
+
+// 逃逸闭包
+var completionHandlers: [() -> Void] = []
+func someFunctionWithEscapingClosure(completionHandler: @escaping () -> Void) {
+    completionHandlers.append(completionHandler)
+}
+
+func someFunctionWithNonescapingClosure(closure: () -> Void) {
+    closure()
+}
+
+class SomeClass {
+    var x = 10
+    func doSomething() {
+        someFunctionWithEscapingClosure {
+            self.x = 100
+        }
+        someFunctionWithNonescapingClosure {
+            x = 200
+        }
+    }
+}
+
+let instance = SomeClass()
+instance.doSomething()
+print(instance.x)
+
+completionHandlers.first?()
+print(instance.x)
+
+// 自动闭包
+var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+print(customersInLine.count)
+
+let customerProvider = { customersInLine.remove(at: 0) }
+print(customersInLine.count)
+
+print("Now serving \(customerProvider())!")
+print(customersInLine.count)
+
+func serve(customer customerProvider: () -> String) {
+    print("Now serving \(customerProvider())!")
+}
+serve(customer: { customersInLine.remove(at: 0) })
+
+func serve(customer customerProvider: @autoclosure () -> String) {
+    print("Now serving \(customerProvider())!")
+}
+serve(customer: customersInLine.remove(at: 0))
+
+var customerProviders: [() -> String] = []
+func collectCustomerProviders(_ customerProvider: @autoclosure @escaping () -> String) {
+    customerProviders.append(customerProvider)
+}
+collectCustomerProviders(customersInLine.remove(at: 0))
+collectCustomerProviders(customersInLine.remove(at: 0))
+print("Cokkected \(customerProviders.count) closures.")
+for customerProvider in customerProviders {
+    print("Now serving \(customerProvider())!")
+}
+
+		
