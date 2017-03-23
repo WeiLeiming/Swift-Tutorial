cb609ef6242cef4eec45c9a8a1d3dbd709792143
WillWei <2008weileiming@163.com>
2017-03-23 11:08:39 +0800

    update Classes and Structures

--- Classes and Structures/Classes and Structures.playground/Contents.swift
+++ Classes and Structures/Classes and Structures.playground/Contents.swift
@@ -5,3 +5,68 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// 定义语法
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+// 类和结构体示例
+let someResolution = Resolution();
+let someVideoMode = VideoMode()
+
+// 属性访问
+print("The width of someResolution is \(someResolution.width)")
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+
+// 结构体类型的成员逐一构造器
+let vga = Resolution(width: 640, height: 480)
+
+// 结构体和枚举是值类型
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+
+cinema.width = 2048
+print("cinema is now \(cinema.width) pixels wide")
+print("hd is still \(hd.width) pixels wide")
+
+enum CompassPoint {
+    case North, South, East, West
+}
+var currentDirection = CompassPoint.West
+let rememberedDirection = currentDirection
+currentDirection = .East
+if rememberedDirection == .West {
+    print("The remembered direction is still .West")
+}
+
+// 类是引用类型
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080p"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+
+// 恒等运算符
+if tenEighty === alsoTenEighty {
+    print("tenEighty and alsoTenEighty refer to the same Resolution instance")
+}
+
+// 指针
+// 类和结构体的选择
+// 字符串、数组、和字典类型的赋值与复制行为
