e41315f1193414e207d7362e47bf52825c1509c6
WillWei <2008weileiming@163.com>
2017-04-13 14:54:48 +0800

    update Subscripts

--- Subscripts/Subscripts.playground/Contents.swift
+++ Subscripts/Subscripts.playground/Contents.swift
@@ -5,3 +5,63 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// 下标语法
+/*
+subscript(index: Int) -> Int {
+    get {
+        // 返回一个适当的Int类型的值
+    }
+    set(newValue) {
+        // 执行适当的赋值操作
+    }
+}
+
+subscript(index: Int) -> Int {
+    // 返回一个适当的Int类型的值
+}
+*/
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+
+// 下标用法
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bid"] = 2
+print(numberOfLegs)
+
+// 下标选项
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+let someValue = matrix[2, 2]
+		
