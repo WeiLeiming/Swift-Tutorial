341752840008c1552fc7cee95059677ba9a3357e
WillWei <2008weileiming@gmail.com>
2017-04-26 15:24:42 +0800

    add Error Handling

---
+++ Error Handling/Error Handling.playground/Contents.swift
@@ -0,0 +1,136 @@
+//: Playground - noun: a place where people can play
+
+// Swift 3.0.1
+
+import UIKit
+
+var str = "Hello, playground"
+
+// 表示并抛出错误
+enum VendingMachineError: Error {
+    case invalidSelection                           // 选择无效
+    case insufficientFunds(coinsNeeded: Int)        // 金额不足
+    case outOfStock                                 // 缺货
+}
+
+throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+// 处理错误
+// 用throwing函数传递错误
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinsDeposited = 0
+    func dispenseSnack(snack: String) {
+        print("Dispensing \(snack)")
+    }
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+        coinsDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Dispensing \(name)")
+    }
+}
+
+let favoriteSnacks = [
+    "Alice": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels"
+]
+
+func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
+}
+
+struct PurchasedSnack {
+    let name: String
+    init(name: String, vendingMachine: VendingMachine) throws {
+        try vendingMachine.vend(itemNamed: name)
+        self.name = name
+    }
+}
+
+// 用Do-Catch处理错误
+var vendingMachine = VendingMachine()
+vendingMachine.coinsDeposited = 8
+do {
+    try buyFavoriteSnack(person: "Alice", vendingMachine: vendingMachine)
+} catch VendingMachineError.invalidSelection {
+    print("Invalid Selection.")
+} catch VendingMachineError.outOfStock {
+    print("Out of Stock.")
+} catch VendingMachineError.insufficientFunds(let coinsNeeded) {
+    print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+}
+
+// 将错误转换成可选值
+func someThrowingFunction() throws -> Int {
+    return 1
+}
+
+let x = try? someThrowingFunction()
+
+let y: Int?
+do {
+    y = try someThrowingFunction()
+} catch {
+    y = nil
+}
+
+func fetchDataFromDisk() throws -> Data? {
+    return nil
+}
+
+func fetchDataFromServer() throws -> Data? {
+    return nil
+}
+
+func fetchData() -> Data? {
+    if let data = try? fetchDataFromDisk() {
+        return data
+    }
+    if let data = try? fetchDataFromServer() {
+        return data
+    }
+    return nil
+}
+
+// 禁用错误传递
+//let photo = try! loadImage(atPath: "./Resources/John Appleseed.jpg")
+
+// 指定清理操作
+/*
+func processFile(filename: String) throws {
+    if exists(filename) {
+        let file = open(filename)
+        defer {
+            close(file)
+        }
+        while let line = try file.readline() {
+            // 处理文件
+        }
+        // close(file)会在这里被调用
+    }
+}
+ */
