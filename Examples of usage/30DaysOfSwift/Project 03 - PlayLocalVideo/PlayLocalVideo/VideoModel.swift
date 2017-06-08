61a1496023043d482d445216c8c2fb0c2a71f15d
WillWei <2008weileiming@gmail.com>
2017-06-08 11:25:47 +0800

    Add VideoModel

---
+++ Examples of usage/30DaysOfSwift/Project 03 - PlayLocalVideo/PlayLocalVideo/VideoModel.swift
@@ -0,0 +1,15 @@
+//
+//  VideoModel.swift
+//  PlayLocalVideo
+//
+//  Created by willwei on 2017/6/8.
+//  Copyright © 2017年 qj-vr. All rights reserved.
+//
+
+import Foundation
+
+struct VideoModel {
+    let image: String
+    let title: String
+    let source: String
+}
