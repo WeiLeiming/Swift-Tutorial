e97d9c3b2b2990e9048f660ebda9e1d8cb30d469
WillWei <2008weileiming@gmail.com>
2017-06-14 17:26:51 +0800

    Modify photo capture

--- Examples of usage/30DaysOfSwift/Project 04 - SnapChatMenu/SnapChatMenu/CameraViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 04 - SnapChatMenu/SnapChatMenu/CameraViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 import AVFoundation
+import Photos
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
 
     // MARK: - Properties
     
-    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var previewView: VideoPreviewView!
     @IBOutlet weak var tempImageView: UIImageView!
 
-    var captureSession : AVCaptureSession?
-    var stillImageOutput : AVCaptureStillImageOutput?
-    var imageOutput: AVCapturePhotoOutput?
-    var previewLayer : AVCaptureVideoPreviewLayer?
+    var captureSession: AVCaptureSession = AVCaptureSession()
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    
+    var isCaptureSessionConfigured = false // Instance proprerty on this view controller class
+    
+    private let sessionQueue = DispatchQueue(label: "session queue",
+                                             attributes: [],
+                                             target: nil) // Communicate with the session and other session objects on this queue.
     
     // MARK: - Memory
     
@@ -32,105 +37,193 @@ class CameraViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
         
-        tempImageView.frame = UIScreen.main.bounds
-        cameraView.frame = UIScreen.main.bounds
-        previewLayer?.frame = cameraView.bounds
+        self.tempImageView.frame = UIScreen.main.bounds
+        self.previewView.frame = UIScreen.main.bounds
+        self.previewView.session = self.captureSession
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        captureSession = AVCaptureSession()
-        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
-        
-        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        var error : NSError?
-        var input: AVCaptureDeviceInput!
-        
-        do {
-            input = try AVCaptureDeviceInput(device: backCamera) }
-        catch let error1 as NSError {
-            error = error1
-            input = nil
-        }
-        
-        if (error == nil && captureSession?.canAddInput(input) != nil) {
-            captureSession?.addInput(input)
-            
-            stillImageOutput = AVCaptureStillImageOutput()
-            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
-            
-            if (captureSession?.canAddOutput(stillImageOutput) != nil) {
-                captureSession?.addOutput(stillImageOutput)
-                
-                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
-                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
-                cameraView.layer.addSublayer(previewLayer!)
-                captureSession?.startRunning()
+        if self.isCaptureSessionConfigured {
+            if !self.captureSession.isRunning {
+                self.captureSession.startRunning()
             }
-            
-        }
-        
-    }
-    
-    func didPressTakePhoto(){
-        
-        if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo){
-            videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
-            stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {
-                (sampleBuffer, error) in
-                
-                if sampleBuffer != nil {
-                    
-                    
-                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                    let dataProvider  = CGDataProvider(data: imageData! as CFData)
-                    let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
-                    
-                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
-                    
-                    self.tempImageView.image = image
-                    self.tempImageView.isHidden = false
+        } else {
+            // First time: request camera access, configure capture session and start it.
+            self.checkCameraAuthorization({ authorized in
+                guard authorized else {
+                    print("Permission to use camera denied.")
+                    return
                 }
-                
-                
+                self.sessionQueue.async {
+                    self.configureCaptureSession({ success in
+                        guard success else { return }
+                        self.isCaptureSessionConfigured = true
+                        self.captureSession.startRunning()
+                        DispatchQueue.main.async {
+                            self.previewView.updateVideoOrientationForDeviceOrientation()
+                        }
+                    })
+                }
             })
         }
-        
-        
     }
     
-    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+    }
+
     var didTakePhoto = Bool()
     
     // MARK: - Action
     
     func didPressTakeAnother(){
         if didTakePhoto == true{
-            tempImageView.isHidden = true
+            self.tempImageView.isHidden = true
             didTakePhoto = false
-            
-        }
-        else{
-            captureSession?.startRunning()
+        } else{
             didTakePhoto = true
-            didPressTakePhoto()
-            
+            snapPhoto()
         }
-        
     }
     
     // MARK: - Touch
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         didPressTakeAnother()
+    }
+    
+    // MARK: - Private Method
+    
+    func checkCameraAuthorization(_ completionHandler: @escaping ((_ authorized: Bool) -> Void)) {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            //The user has previously granted access to the camera.
+            completionHandler(true)
+            
+        case .notDetermined:
+            // The user has not yet been presented with the option to grant video access so request access.
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { success in
+                completionHandler(success)
+            })
+            
+        case .denied:
+            // The user has previously denied access.
+            completionHandler(false)
+            
+        case .restricted:
+            // The user doesn't have the authority to request access e.g. parental restriction.
+            completionHandler(false)
+        }
+    }
+    
+    func checkPhotoLibraryAuthorization(_ completionHandler: @escaping ((_ authorized: Bool) -> Void)) {
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized:
+            // The user has previously granted access to the photo library.
+            completionHandler(true)
+            
+        case .notDetermined:
+            // The user has not yet been presented with the option to grant photo library access so request access.
+            PHPhotoLibrary.requestAuthorization({ status in
+                completionHandler((status == .authorized))
+            })
+            
+        case .denied:
+            // The user has previously denied access.
+            completionHandler(false)
+            
+        case .restricted:
+            // The user doesn't have the authority to request access e.g. parental restriction.
+            completionHandler(false)
+        }
+    }
+    
+    func defaultDevice() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInDuoCamera,
+                                                      mediaType: AVMediaTypeVideo,
+                                                      position: .back) {
+            return device // use dual camera on supported devices
+        } else if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera,
+                                                             mediaType: AVMediaTypeVideo,
+                                                             position: .back) {
+            return device // use default back facing camera otherwise
+        } else {
+            fatalError("All supported devices are expected to have at least one of the queried capture devices.")
+        }
+    }
+    
+    func configureCaptureSession(_ completionHandler: ((_ success: Bool) -> Void)) {
+        var success = false
+        defer { completionHandler(success) } // Ensure all exit paths call completion handler.
+        
+        // Get video input for the default camera.
+        let videoCaptureDevice = defaultDevice()
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else {
+            print("Unable to obtain video input for default camera.")
+            return
+        }
+        
+        // Create and configure the photo output.
+        let capturePhotoOutput = AVCapturePhotoOutput()
+        capturePhotoOutput.isHighResolutionCaptureEnabled = true
+        capturePhotoOutput.isLivePhotoCaptureEnabled = capturePhotoOutput.isLivePhotoCaptureSupported
+        
+        // Make sure inputs and output can be added to session.
+        guard self.captureSession.canAddInput(videoInput) else { return }
+        guard self.captureSession.canAddOutput(capturePhotoOutput) else { return }
+        
+        // Configure the session.
+        self.captureSession.beginConfiguration()
+        self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        self.captureSession.addInput(videoInput)
+        self.captureSession.addOutput(capturePhotoOutput)
+        self.captureSession.commitConfiguration()
+        
+        self.capturePhotoOutput = capturePhotoOutput
+        
+        success = true
+    }
+    
+    // MARK: - Capturing a Photo
+    
+    func snapPhoto() {
+        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+        let videoPreviewLayerOrientation = previewView.videoPreviewLayer.connection.videoOrientation
+        self.sessionQueue.async {
+            // Update the photo output's connection to match the video orientation of the video preview layer.
+            if let photoOutputConnection = capturePhotoOutput.connection(withMediaType: AVMediaTypeVideo) {
+                photoOutputConnection.videoOrientation = videoPreviewLayerOrientation
+            }
+            
+            let photoSettings = AVCapturePhotoSettings()
+            photoSettings.isAutoStillImageStabilizationEnabled = true
+            photoSettings.isHighResolutionPhotoEnabled = true
+            photoSettings.flashMode = .auto
+            
+            capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
+        }
+    }
+    
+    // MARK: - AVCapturePhotoCaptureDelegate
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        guard error == nil, let jpegData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+                print("Unable to create JPEG data.")
+                return
+        }
+        self.tempImageView.image = UIImage(data: jpegData)
+        self.tempImageView.isHidden = false
+    }
+    
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishCaptureForResolvedSettings resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
+        print("capture finish.")
     }
 
 }
---
+++ Examples of usage/30DaysOfSwift/Project 04 - SnapChatMenu/SnapChatMenu/VideoPreviewView.swift
@@ -0,0 +1,44 @@
+//
+//  VideoPreviewView.swift
+//  SnapChatMenu
+//
+//  Created by willwei on 2017/6/14.
+//  Copyright © 2017年 qj-vr. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPreviewView: UIView {
+
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPreviewLayer.session }
+        set { videoPreviewLayer.session = newValue }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    private var orientationMap: [UIDeviceOrientation : AVCaptureVideoOrientation] = [
+        .portrait           : .portrait,
+        .portraitUpsideDown : .portraitUpsideDown,
+        .landscapeLeft      : .landscapeRight,
+        .landscapeRight     : .landscapeLeft,
+        ]
+    
+    func updateVideoOrientationForDeviceOrientation() {
+        if let videoPreviewLayerConnection = videoPreviewLayer.connection {
+            let deviceOrientation = UIDevice.current.orientation
+            guard let newVideoOrientation = orientationMap[deviceOrientation],
+                deviceOrientation.isPortrait || deviceOrientation.isLandscape
+                else { return }
+            videoPreviewLayerConnection.videoOrientation = newVideoOrientation
+        }
+    }
+
+}
