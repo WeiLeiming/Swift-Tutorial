25428fda20cd268bd461614b145733fbe17e4941
WillWei <2008weileiming@gmail.com>
2017-06-23 17:19:16 +0800

    Base Controller

--- Examples of usage/30DaysOfSwift/Project 06 - FindMyLocation/FindMyLocation/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 06 - FindMyLocation/FindMyLocation/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    // MARK: - Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: - Memory
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Status Bar
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    // MARK: - Button Action
 
+    @IBAction func findMyLocationButtonDidTouch(_ sender: UIButton) {
+    }
 
 }
 
