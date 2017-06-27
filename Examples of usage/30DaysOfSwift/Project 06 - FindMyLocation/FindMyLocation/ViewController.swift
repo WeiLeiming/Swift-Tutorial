b98cc7b9bdd556e7135cc4bfb6d90cd147bc056f
WillWei <2008weileiming@gmail.com>
2017-06-27 15:29:39 +0800

    Add location function

--- Examples of usage/30DaysOfSwift/Project 06 - FindMyLocation/FindMyLocation/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 06 - FindMyLocation/FindMyLocation/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     // MARK: - IBOutlet
     
     @IBOutlet weak var locationLabel: UILabel!
     
-    // MARK: - Load
+    // MARK: - Properties
+    
+    var locationManager: CLLocationManager?
+    
+    // MARK: - Load View
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +42,51 @@ class ViewController: UIViewController {
     // MARK: - Button Action
 
     @IBAction func findMyLocationButtonDidTouch(_ sender: UIButton) {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.startUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            guard error == nil else {
+                self.locationLabel.text = "Reverse geocoder failed with error" + error!.localizedDescription
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                let placemark = placemarks?.first
+                self.convertLocationInfo(placemark)
+            } else {
+                self.locationLabel.text = "Problem with the data received from geocoder"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.locationLabel.text = "Error while updating location " + error.localizedDescription
+    }
+    
+    // MARK: - Private Method
+    
+    private func convertLocationInfo(_ placemark: CLPlacemark?) {
+        guard placemark != nil else {
+            return
+        }
+        
+        locationManager?.stopUpdatingLocation()
+        
+        let name = (placemark?.name != nil) ? placemark?.name : ""
+        let subLocality = (placemark?.subLocality != nil) ? placemark?.subLocality : ""
+        let locality = (placemark?.locality != nil) ? placemark?.locality : ""
+        let administrativeArea = (placemark?.administrativeArea != nil) ? placemark?.administrativeArea : ""
+        let country = (placemark?.country != nil) ? placemark?.country : ""
+        
+        self.locationLabel.text = name! + " " + subLocality!
+        self.locationLabel.text?.append("\n" + locality! + ", " + administrativeArea! + ", " + country!)
     }
 
 }
