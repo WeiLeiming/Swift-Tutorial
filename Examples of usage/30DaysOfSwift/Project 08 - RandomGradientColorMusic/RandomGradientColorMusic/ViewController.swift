2c1e5ebcef812aab05dce9af79e0e4849ef2c107
WillWei <2008weileiming@gmail.com>
2017-07-05 11:34:37 +0800

    Add interface

--- Examples of usage/30DaysOfSwift/Project 08 - RandomGradientColorMusic/RandomGradientColorMusic/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 08 - RandomGradientColorMusic/RandomGradientColorMusic/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - View Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: - Memory
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Action
 
+    @IBAction func playMusicButtonDidTouch(_ sender: UIButton) {
+    }
 
 }
 
