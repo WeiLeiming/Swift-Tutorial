407a7724ffb3338f187fb31b64b26372a8abfe80
WillWei <2008weileiming@gmail.com>
2017-07-19 15:30:52 +0800

    Add zoom

--- Examples of usage/30DaysOfSwift/Project 09 -ImageScroller/ImageScroller/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 09 -ImageScroller/ImageScroller/ViewController.swift
@@ -8,18 +8,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    // MARK: - Properties
+    
+    var imageView: UIImageView!
+    var scrollView: UIScrollView!
+    
+    // MARK: - View Load
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initScrollView()
+        
+        setZoomScale()
+        scrollView.zoomScale = scrollView.minimumZoomScale
+        
+        recenterImage()
     }
+    
+    // MAKR: - Memory
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Status Bar
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Layout
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        setZoomScale()
+        
+        if scrollView.zoomScale < scrollView.minimumZoomScale {
+            scrollView.zoomScale = scrollView.minimumZoomScale
+        }
+        
+        recenterImage()
+    }
+    
+    // MARK: - Private Method
+    
+    private func initScrollView() {
+        imageView = UIImageView(image: #imageLiteral(resourceName: "Steve"))
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.backgroundColor = UIColor.clear
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.delegate = self
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+    }
+    
+    private func setZoomScale() {
+        let scrollViewSize = scrollView.bounds.size
+        let imageViewSize = imageView.bounds.size
+        
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = min(widthScale, heightScale)
+        scrollView.maximumZoomScale = 3.0
+    }
+    
+    private func recenterImage() {
+        let scrollViewSize = scrollView.bounds.size
+        let imageViewSize = imageView.frame.size
+        
+        let horizontalSpace = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width) / 2.0 : 0
+        let verticalSpace = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height) / 2.0 : 0
+        
+        scrollView.contentInset = UIEdgeInsetsMake(verticalSpace, horizontalSpace, verticalSpace, horizontalSpace)
+    }
+    
+    // MARK: - UIScrollViewDelegate
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        recenterImage()
+    }
 
 }
 
