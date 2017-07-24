f8f69eab0ff06e170505d59072441c302c9cbca7
WillWei <2008weileiming@gmail.com>
2017-07-24 11:32:57 +0800

    Add interface

--- Examples of usage/30DaysOfSwift/Project 10 - VideoBackground/VideoBackground/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 10 - VideoBackground/VideoBackground/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - View Load
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: - Memory
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Status Bar
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
