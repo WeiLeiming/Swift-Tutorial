aa49fd1573a8a8241e3e059d6be5e29c55fd48cf
WillWei <2008weileiming@gmail.com>
2017-07-24 14:50:03 +0800

    Add player

--- Examples of usage/30DaysOfSwift/Project 10 - VideoBackground/VideoBackground/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 10 - VideoBackground/VideoBackground/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVKit
+import MediaPlayer
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let avplayerVC = AVPlayerViewController()
+    
+    
     // MARK: - View Load
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        avplayerVC.view.frame = view.frame
+        avplayerVC.showsPlaybackControls = false
+        view.addSubview(avplayerVC.view)
+        view.sendSubview(toBack: avplayerVC.view)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupBackgroundVideo()
+    }
+    
+    // MARK: - Deinit
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - Memory
@@ -28,6 +50,29 @@ class ViewController: UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    // MARK: - Orientation
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    // MARK: - Video
+    
+    private func setupBackgroundVideo() {
+        let url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "moments", ofType: "mp4")!)
+        
+        avplayerVC.player = AVPlayer(url: url)
+        avplayerVC.player?.play()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.playerItemDidPlayEnd), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: avplayerVC.player?.currentItem)
+    }
+    
+    // MARK: - Notification
+    
+    func playerItemDidPlayEnd() {
+        avplayerVC.player?.seek(to: kCMTimeZero)
+        avplayerVC.player?.play()
     }
 
 
