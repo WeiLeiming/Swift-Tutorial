ddaac429106657d58faf13973d014170049f2954
WillWei <2008weileiming@gmail.com>
2017-08-07 15:15:14 +0800

    Add picker view

--- Examples of usage/30DaysOfSwift/Project 14 - EmojiSlotMachine/EmojiSlotMachine/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 14 - EmojiSlotMachine/EmojiSlotMachine/ViewController.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // MARK: - IBOutlet
 
+    @IBOutlet weak var emojiPickerView: UIPickerView!
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    var imageArray = [String]()
+    var dataArray1 = [Int]()
+    var dataArray2 = [Int]()
+    var dataArray3 = [Int]()
+    
+    // MARK: - View Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageArray = ["👻","👸","💩","😘","🍔","🤖","🍟","🐼","🚖","🐷"]
+        for _ in 0..<100 {
+            dataArray1.append(Int(arc4random_uniform(10)))
+            dataArray2.append(Int(arc4random_uniform(10)))
+            dataArray3.append(Int(arc4random_uniform(10)))
+        }
     }
+    
+    // MARK: - Memory
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Status Bar
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: - Action
 
+    @IBAction func goButtoDidTouch(_ sender: UIButton) {
+    }
+    
+    // MARK: - UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerLabel = UILabel()
+        
+        if component == 0 {
+            pickerLabel.text = imageArray[dataArray1[row]]
+        } else if component == 1 {
+            pickerLabel.text = imageArray[dataArray2[row]]
+        } else {
+            pickerLabel.text = imageArray[dataArray3[row]]
+        }
+        
+        pickerLabel.font = UIFont(name: "Apple Color Emoji", size: 80)
+        pickerLabel.textAlignment = .center
+        
+        return pickerLabel
+    }
 
 }
 
