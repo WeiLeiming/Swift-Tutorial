fe20db68fe90bad95a7b995f1498ef18e6b5cb4a
WillWei <2008weileiming@gmail.com>
2017-08-31 11:30:12 +0800

    Add animation

--- Examples of usage/30DaysOfSwift/Project 14 - EmojiSlotMachine/EmojiSlotMachine/ViewController.swift
+++ Examples of usage/30DaysOfSwift/Project 14 - EmojiSlotMachine/EmojiSlotMachine/ViewController.swift
@@ -22,18 +22,33 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     var dataArray1 = [Int]()
     var dataArray2 = [Int]()
     var dataArray3 = [Int]()
+    var bounds: CGRect = CGRect.zero
     
     // MARK: - View Load
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bounds = goButton.bounds
         imageArray = ["👻","👸","💩","😘","🍔","🤖","🍟","🐼","🚖","🐷"]
         for _ in 0..<100 {
             dataArray1.append(Int(arc4random_uniform(10)))
             dataArray2.append(Int(arc4random_uniform(10)))
             dataArray3.append(Int(arc4random_uniform(10)))
         }
+        resultLabel.text = ""
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        goButton.alpha = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: { 
+            self.goButton.alpha = 1
+        }, completion: nil)
     }
     
     // MARK: - Memory
@@ -52,6 +67,24 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     // MARK: - Action
 
     @IBAction func goButtoDidTouch(_ sender: UIButton) {
+        emojiPickerView.selectRow(Int(arc4random_uniform(90)) + 3, inComponent: 0, animated: true)
+        emojiPickerView.selectRow(Int(arc4random_uniform(90)) + 3, inComponent: 1, animated: true)
+        emojiPickerView.selectRow(Int(arc4random_uniform(90)) + 3, inComponent: 2, animated: true)
+        let select0 = dataArray1[emojiPickerView.selectedRow(inComponent: 0)]
+        let select1 = dataArray2[emojiPickerView.selectedRow(inComponent: 1)]
+        let select2 = dataArray3[emojiPickerView.selectedRow(inComponent: 2)]
+        if select0 == select1 && select1 == select2 {
+            resultLabel.text = "Bingo!"
+        } else {
+            resultLabel.text = "💔"
+        }
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 5, options: .curveLinear, animations: { 
+            self.goButton.bounds = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: self.bounds.size.width + 20, height: self.bounds.size.height)
+        }) { (complete) in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut, animations: { 
+                self.goButton.bounds = self.bounds
+            }, completion: nil)
+        }
     }
     
     // MARK: - UIPickerViewDataSource
